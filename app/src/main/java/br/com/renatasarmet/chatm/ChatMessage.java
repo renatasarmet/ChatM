66c910f4224292f0e6c9462f005ee54c65cae1a4
renatasarmet <renatassmendes@hotmail.com>
2016-11-20 15:54:23 -0200

    Copiado código video

---
+++ app/src/main/java/br/com/renatasarmet/chatm/ChatMessage.java
@@ -0,0 +1,16 @@
+package br.com.renatasarmet.chatm;
+
+//package com.google.firebase.zerotoapp;
+
+public class ChatMessage {
+    public String name;
+    public String message;
+
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+}
